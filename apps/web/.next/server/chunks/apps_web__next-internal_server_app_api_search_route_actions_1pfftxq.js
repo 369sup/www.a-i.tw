7c2413901b2633edc0646cf8a28a7e1d21e33e8d
369sup <sup@a-i.tw>
2026-07-11 17:29:15 +0800

    chore: stop tracking generated build artifacts

--- apps/web/.next/server/chunks/apps_web__next-internal_server_app_api_search_route_actions_1pfftxq.js
+++
@@ -1,3 +0,0 @@
-module.exports=[18798,(e,o,d)=>{}];
-
-//# sourceMappingURL=apps_web__next-internal_server_app_api_search_route_actions_1pfftxq.js.map
--- apps/web/.next/server/chunks/ssr/apps_web__next-internal_server_app_docs_[[___slug]]_page_actions_0vqswuh.js
+++
@@ -1,3 +0,0 @@
-module.exports=[57038,(a,b,c)=>{}];
-
-//# sourceMappingURL=apps_web__next-internal_server_app_docs_%5B%5B___slug%5D%5D_page_actions_0vqswuh.js.map
